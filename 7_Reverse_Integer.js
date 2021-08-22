a9e92cbe8756ab56954afe76f28681dbf4488fc1
coltenappleby <colten.appleby@gmail.com>
2021-08-22 18:00:47 -0400

    Completed

--- 7_Reverse_Integer.js
+++ 7_Reverse_Integer.js
@@ -2,11 +2,13 @@ var reverse = function(x) {
     let multi = 1
     if (x < 0){
         multi = -1
-    }
-    return multi * parseInt(x.toString().split('').reverse().join(''))
+    };
+    let ans = multi * parseInt(x.toString().split('').reverse().join(''));
+    return ((ans > (2**31 -1)) || (ans < (-(2**31))))  ? 0 : ans;
 };
 
-reverse(123)
 
-reverse(1534236469) 
-// Output 9646324351  // Expected 0 //   1027/1032 Questions Passed
+reverse(123) // #=> 321
+
+// Runtime: 80 ms, faster than 94.10% of JavaScript online submissions for Reverse Integer.
+// Memory Usage: 40.4 MB, less than 59.24% of JavaScript online submissions for Reverse Integer.
