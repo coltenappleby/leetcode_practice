191f6831b0cb10b8ee4253c1a3dc85284d1fd15b
coltenappleby <colten.appleby@gmail.com>
2021-09-13 21:40:54 -0400

    i was off the mark. needed to put balloon in an object

--- 1189_maximum_number_of_balloons.js
+++ 1189_maximum_number_of_balloons.js
@@ -7,20 +7,38 @@
     for(let i = 0; i < text.length; i++){
         if(!textObj[text[i]]){
             textObj[text[i]] = 1
+        } else {
+            textObj[text[i]] += 1
         }
-        textObj[text[i]] += 1  
     }
-    
+
     let balloon = "balloon"
+    let balloonObj = {}
+    for(let i = 0; i < balloon.length; i++){
+        if(!balloonObj[balloon[i]]){
+            balloonObj[balloon[i]] = 1
+        } else {
+            balloonObj[balloon[i]] += 1
+        }
+    }
+
     let maxNum = 10e4-1
     for(let i = 0; i < balloon.length; i++){
         if(textObj[balloon[i]] === undefined){
             return 0;
-        } else if(textObj[balloon[i]] < maxNum){
-            maxNum = textObj[balloon[i]]
-        } else {      
+        } else if(textObj[balloon[i]]/balloonObj[balloon[i]] < maxNum){
+            maxNum = Math.floor(textObj[balloon[i]]/balloonObj[balloon[i]])
+        } else {    
             continue;
         }
     }
-    
+    return maxNum;
 };
+
+// maxNumberOfBalloons("nlaebolko")
+// maxNumberOfBalloons("ballon")
+maxNumberOfBalloons("krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw")
+
+// failing on "nlaebolko" -- returning undefined
+// it is not accounting for double letters
+// maybe remove letters from the object? - remove maxNums from text obj
