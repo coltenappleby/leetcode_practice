0bdd48782eb3370ad8cc5f675a009941d6350a2c
Colten Appleby <colten.appleby@gmail.com>
2021-10-24 23:47:54 -0400

    making add_two-nums faster. close

--- 2_add_two_nums.rb
+++ 2_add_two_nums.rb
@@ -9,7 +9,7 @@ end
 # @param {ListNode} l1
 # @param {ListNode} l2
 # @return {ListNode}
-def add_two_numbers(l1, l2)
+def add_two_numbers_old(l1, l2)
     num1 = 0
     num2 = 0 
     count1 = 0
@@ -43,6 +43,9 @@ def add_two_numbers(l1, l2)
     end
 
     return result
+
+    # Runtime: 175 ms, faster than 12.79% of Ruby online submissions for Add Two Numbers.
+    # Memory Usage: 210.2 MB, less than 54.13% of Ruby online submissions for Add Two Numbers
 end
 
 list1node1 = ListNode.new(3)
@@ -52,6 +55,34 @@ list1node3 = ListNode.new(2, list1node2)
 list2node1 = ListNode.new(4)
 list2node2 = ListNode.new(6, list2node1)
 list2node3 = ListNode.new(5, list2node2)
+
+
+
+
+def add_two_numbers(l1, l2)
+    node = ListNode.new()
+    result = node
+    carry = 0
+    while(l1 != nil || l2 != nil)
+        l1.val != nil ? x = l1.val : x = 0
+        l2.val != nil ? x = l2.val : x = 0
+
+        sum = l1.val + l2.val + carry
+        if(sum >= 10)
+            carry = 1
+            sum = sum-10
+        end
+        
+        node.val = sum
+        node.next = ListNode.new()
+        
+        l1 != nil ? l1 = l1.next : l1 = nil
+        l2 != nil ? l2 = l2.next : l2 = nil
+        carry = 0
+    end
+    return node.next.next.val
+    
+end
 
 
 puts add_two_numbers(list1node3, list2node3)
